32ce4e2586687bd5c33a21f584d612793ff128dd
Atiger77 <atiger77@126.com>
2017-01-28 00:21:14 +0800

    #Add main function

--- ChapterOne/PortScan.py
+++ ChapterOne/PortScan.py
@@ -12,16 +12,27 @@ Desc:端口检测脚本，检测目标主机是否开放常用端口
 
 import socket,os,sys
 
-def portscan():
-    port_dict = {'FTP':20,'SSH':22,'telnet':23,'smtp':25,'DNS':53,'HTTP':80,'HTTPS':443,'Mysql':3306,'Redis':6379,'Jenkins':8080,'elasticsearch':9200,'zabbix':10050,'MongoDB':27017} 
-    
-    for i in port_dict.keys():
+def portscan(ip,port):
+    try:    
         socket.setdefaulttimeout(2)
         s = socket.socket()
-        s.connect(('127',i))    
+        s.connect((ip,port))    
+        banner = s.recv(1024)
+        print banner
+        return banner
+    except:
+        return
 
+def main():
+    port_dict = {'FTP':20,'SSH':22,'telnet':23,'smtp':25,'DNS':53,'HTTP':80,'HTTPS':443,'Mysql':3306,'Redis':6379,'Jenkins':8080,'elasticsearch':9200,'zabbix':10050,'MongoDB':27017} 
+    for i in range(1,256):
+        ip = '172.16.56.' + str(i)
+        for j in port_dict.values():
+            banner = portscan(ip,j)
+            if banner:
+                print '[+] ' + ip + ' : ' + banner    
 
 
 if __name__ == '__main__':
-   portscan
+   main()
 
